36551323db0ee870d855a33efaf579349deb7703
shruti <shruti.shinde1210@gmail.com>
2023-05-09 16:07:46 +0530

    Hidden form field

---
+++ src/About.java
@@ -0,0 +1,39 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/About")
+public class About extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+
+		String name=request.getParameter("uname");
+		String pass=request.getParameter("upass");
+
+		out.print("welcome to about us : "+name+" "+pass);
+
+		out.print("<form action='Profile'>");
+		out.print("<input type='hidden' name='uname' value='"+name+"'>");
+		out.print("<input type='hidden' name='upass' value='"+pass+"'>");
+		out.print("<input type='submit' value='click'>");
+		out.print("</form>");
+
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
---
+++ src/Profile.java
@@ -0,0 +1,33 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/Profile")
+public class Profile extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+
+		String name=request.getParameter("uname");
+		String pass=request.getParameter("upass");
+
+		out.print("welcome to about us : "+name+" "+pass);
+
+	
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
